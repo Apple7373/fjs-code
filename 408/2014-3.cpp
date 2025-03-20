4dba46c930b3d144c424cfd6b55f97ccbc181200
fjs <1039953290@qq.com>
2025-03-20 10:39:47 +0800

    第六次提交，T-n题目放到了代码的开头

--- 408/2014-3.cpp
+++ 408/2014-3.cpp
@@ -9,7 +9,8 @@ int main()
 	for (int i = 0; i < s.size(); i++) {
 		if (s[i] == '(') {
 			st.push(s[i]); // 左括号进栈
-			m = max(m, st.size());  // 更新栈的最大 大小
+			int curm = st.size(); 
+			m = max(m, curm);  // 更新栈的最大 大小
 		}
 		else if (s[i] == ')') st.pop();  // 遇到右括号 栈顶一定是左括号   括号一定是匹配的
 	}
--- 408/T-1.cpp
+++ 408/T-1.cpp
@@ -1,42 +1,42 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-	int a;
 	vector<int> nums;
 	nums.push_back(0);
+	int a;
 	while (cin >> a) {
 		nums.push_back(a);
 		if (getchar() == '\n') break;
 	}
 	int n = nums.size()-1;
-	// idx:1~n
-	vector<int> v[n+10];
-	for (int i = 1; i < n; i++) {
-		// pass
-		if (i != 1 && v[i].size() == 0) {
+	// idx: 1~n 
+	vector<int> v[n+5];
+	
+	// 往篮子里面加数
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < nums[i] && i+j+1 <= n; j++) {
+			v[i+j+1].push_back(i);
+		}
+	}
+	// 判断是否为None
+
+	// 从篮子中找一个合理的路径
+	vector<int> ans;
+	ans.push_back(n); // 加入终点
+	while (ans.back() != 1) {
+		// 篮子下标 ans.back();
+		if (v[ans.back()].size() == 0) {
 			cout << "None" << endl;
 			return 0;
 		}
-		for (int j = 1; j <= nums[i] && i+j <= n; j++) {
-			v[i+j].push_back(i);
-		}
-	}
-	
-	if (v[n].size() == 0) {
-		cout << "None" << endl;
-		return 0;
-	}
-	vector<int> ans;
-	ans.push_back(n);
-	ans.push_back(v[n][0]);
-	while (ans.back() != 1) {
 		ans.push_back(v[ans.back()][0]);
 	}
-	
-	for (int i = ans.size()-1; i >= 0; i--)
+
+	for (int i = ans.size()-1; i >= 0; i--)	
 		cout << ans[i] << ' ';
-	cout << endl;
+	
+		
+	return 0;
 }
