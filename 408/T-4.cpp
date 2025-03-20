8572e67f9c93700effd8905dc6194bc53aaa65ba
fjs <95755778+Apple7373@users.noreply.github.com>
2025-03-20 10:57:08 +0800

    Update T-4.cpp

--- 408/T-4.cpp
+++ 408/T-4.cpp
@@ -1,5 +1,23 @@
 #include <bits/stdc++.h>
+/*
 
+3、输入学生个数以及每个学生的姓名和3门课程成绩：输出不及格学生的信息；按平均成绩排序，从高到低输出学生信息。
+输入示例：
+5
+zhaoyi   70 80 91
+zhanger  68 40 90
+zhangsan 60 70 80
+lisi      70 80 90
+wangwu  52 70 100 
+
+输出：
+*name:zhaoyi    score:70 80 91 80.3333
+*name:lisi      score:70 80 90 80
+*name:wangwu    score:52 70 100 74
+*name:zhangsan  score:60 70 80 70
+*name:zhanger   score:68 40 90 66
+
+*/
 using namespace std;
 struct Student{
     string name;
